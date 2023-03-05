881b4e4a5c7ca8c94e34f329ceb2a60de03d814c
Souvik Mitra <msouvik38@gmail.com>
2023-03-05 21:54:28 +0530

    color format changes

--- src/components/Certification/index.js
+++ src/components/Certification/index.js
@@ -1,76 +1,90 @@
-import React, { useEffect, useState } from "react";
-import Loader from "react-loaders";
-import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../../firebase'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
-const Certification = () => { 
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const [certificate, setCertificate] = useState([]);
+const Certification = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const [certificate, setCertificate] = useState([])
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
-        return () => {
-            clearTimeout(timer);
-        }
-    });
-
-    useEffect(() => {
-        getCertificate();
-    }, []);
-
-    const getCertificate = async () => {
-        const querySnapshot = await getDocs(collection(db, 'certification'));
-        setCertificate(querySnapshot.docs.map((doc) => doc.data()));
+    return () => {
+      clearTimeout(timer)
     }
+  })
 
-    const renderCertificate = (certificate) => {
-        return (
-            <div className="images-container">
-                {
-                    certificate.map((port, idx) => {
-                        return (
-                            <div className="image-box" key={idx}>
-                                <img 
-                                src={port.image}
-                                className="portfolio-image"
-                                alt="certificate" />
-                                <div className="content">
-                                    <p className="title">{port.name}</p>
-                                    <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        );
-    }
+  useEffect(() => {
+    getCertificate()
+  }, [])
 
+  const getCertificate = async () => {
+    const querySnapshot = await getDocs(collection(db, 'certification'))
+    setCertificate(querySnapshot.docs.map((doc) => doc.data()))
+  }
 
+  const renderCertificate = (certificate) => {
     return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Certification".split("")}
-                        idx={15}
-                    />
-                </h1>
-                <div>{renderCertificate(certificate)}</div>
+      <div className="images-container">
+        {certificate.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.image}
+                className="portfolio-image"
+                alt="certificate"
+              />
+              <div className="content">
+                <p
+                  className="title"
+                  style={{ 'font-family': 'Coolvetica', color: '#ffd700' }}
+                >
+                  {port.name}
+                </p>
+                <h4
+                  className="description"
+                  style={{ 'font-family': 'Coolvetica', color: '#ffd700' }}
+                >
+                  {port.description}
+                </h4>
+                <button
+                  className="btn"
+                  style={{ 'font-family': 'Coolvetica' }}
+                  onClick={() => window.open(port.url)}
+                >
+                  View &nbsp;
+                  <FontAwesomeIcon icon={faUpRightFromSquare} />
+                </button>
+              </div>
             </div>
-            <Loader type="pacman" />
-        </>
-    );
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Certification'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderCertificate(certificate)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Certification;
+export default Certification
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,76 +1,88 @@
-import React, { useEffect, useState } from "react";
-import Loader from "react-loaders";
-import AnimatedLetters from "../AnimatedLetters";
-import "./index.scss";
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../../firebase';
+import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../../firebase'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
-const Project = () => { 
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const [project, setProject] = useState([]);
+const Project = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const [project, setProject] = useState([])
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
-        return () => {
-            clearTimeout(timer);
-        }
-    });
-
-    useEffect(() => {
-        getProject();
-    }, []);
-
-    const getProject = async () => {
-        const querySnapshot = await getDocs(collection(db, 'project'));
-        setProject(querySnapshot.docs.map((doc) => doc.data()).sort((doc1, doc2) => doc2.id - doc1.id));
+    return () => {
+      clearTimeout(timer)
     }
+  })
 
-    const renderProject = (project) => {
-        return (
-            <div className="images-container">
-                {
-                    project.map((port, idx) => {
-                        return (
-                            <div className="image-box" key={idx}>
-                                <img 
-                                src={port.image}
-                                className="portfolio-image"
-                                alt="project" />
-                                <div className="content">
-                                    <p className="title">{port.name}</p>
-                                    <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        );
-    }
+  useEffect(() => {
+    getProject()
+  }, [])
 
+  const getProject = async () => {
+    const querySnapshot = await getDocs(collection(db, 'project'))
+    setProject(
+      querySnapshot.docs
+        .map((doc) => doc.data())
+        .sort((doc1, doc2) => doc2.id - doc1.id)
+    )
+  }
 
+  const renderProject = (project) => {
     return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Project".split("")}
-                        idx={15}
-                    />
-                </h1>
-                <div>{renderProject(project)}</div>
+      <div className="images-container">
+        {project.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img src={port.image} className="portfolio-image" alt="project" />
+              <div className="content">
+                <p className="title" style={{ 'font-family': 'Coolvetica', 'color': '#ffd700' }}>
+                  {port.name}
+                </p>
+                <h4
+                  className="description"
+                  style={{ 'font-family': 'Coolvetica', 'color': '#ffd700' }}
+                >
+                  {port.description}
+                </h4>
+                <button
+                  className="btn"
+                  style={{ 'font-family': 'Coolvetica' }}
+                  onClick={() => window.open(port.url)}
+                >
+                  View
+                  &nbsp;
+                  <FontAwesomeIcon icon={faUpRightFromSquare} />
+                </button>
+              </div>
             </div>
-            <Loader type="pacman" />
-        </>
-    );
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Project'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderProject(project)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Project;
+export default Project
